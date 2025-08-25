76c78bcc7b76d3a00f12a1954167130ab8ee21a2
Ciznia <gabriel.hosquet@epitech.eu>
2025-08-25 09:24:24 +0200

    Adapt to new NixOS installation

--- hardware/cizchine.hardware-configuration.nix
+++ hardware/cizchine.hardware-configuration.nix
@@ -5,40 +5,35 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot = {
-    initrd = {
-      availableKernelModules = [ "vmd" "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-      kernelModules = [ ];
-    };
-    kernelModules = [ "kvm-intel" ];
-    kernelPackages = pkgs.linuxPackages_latest;
-    extraModulePackages = [ ];
-  };
+  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "nvme" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/4893a9d2-7a49-4d64-b57b-79825de8806b";
+    { device = "/dev/disk/by-uuid/0a2c2c97-67cc-44df-ac0f-e3aa58113f38";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/E9E8-B9B9";
+    { device = "/dev/disk/by-uuid/90C5-BD40";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/03cff620-0ce8-437b-95ac-b90dc6a7678f"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
